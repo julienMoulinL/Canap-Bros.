cc7bf8b73d2af3aa205154baac78aacf2c4c35a8
julien latos <julienlatos.jl@gmail.com>
2024-05-15 16:38:23 +0200

    first commit

---
+++ tp-final-code/react.js
@@ -0,0 +1,19 @@
+function toggleMenu() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+// Ajouter la classe "active" à l'élément de la barre de navigation correspondant à la page actuelle
+var currentLocation = window.location.href;
+var navLinks = document.querySelectorAll('.topnav a');
+navLinks.forEach(function(link) {
+  if (link.href === currentLocation) {
+    link.classList.add('active');
+  } else {
+    link.classList.remove('active');
+  }
+});
